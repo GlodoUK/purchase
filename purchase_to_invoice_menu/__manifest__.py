ef78c5333229f6c7fbc85fc7c687364f9bc4a786
Karl Southern <karl@theangryangel.co.uk>
2022-04-04 11:54:32 +0100

    feat(purchase_to_invoice_menu): adds a 'to invoice' menu to the purchasing app

---
+++ purchase_to_invoice_menu/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "purchase_to_invoice_menu",
+    "summary": "Adds a 'To Invoice' menu to the Purchase menu",
+    "author": "Glo Networks",
+    "website": "https://www.glo.systems/",
+    "category": "Uncategorized",
+    "version": "13.0.1.0.0",
+    "depends": ["purchase"],
+    "data": [
+        "views/menu.xml",
+    ],
+    "demo": [],
+    "license": "AGPL-3",
+}
