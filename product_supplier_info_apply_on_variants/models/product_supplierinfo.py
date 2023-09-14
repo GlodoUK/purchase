0e413852615befd6cbeac00b6bc8998dc3fc498b
Karl <karl@theangryangel.co.uk>
2023-09-14 16:27:18 +0100

    Update product_supplierinfo.py

--- product_supplier_info_apply_on_variants/models/product_supplierinfo.py
+++ product_supplier_info_apply_on_variants/models/product_supplierinfo.py
@@ -4,13 +4,32 @@ from odoo import api, fields, models
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
+    def _match_all_variant_values(self, product_template_attribute_value_ids):
+        """ It currently checks that all variant values (`product_template_attribute_value_ids`)
+        are in the product (`self`).
+
+        If multiple values are encoded for the same attribute line, only one of
+        them has to be found on the variant.
+        """
+        self.ensure_one()
+
+
     def _prepare_sellers(self, params=False):
         self.ensure_one()
         res = super()._prepare_sellers(params)
         to_remove = self.env["product.supplierinfo"]
 
         for record in res.filtered(lambda p: p.apply_on_ptav_ids and not p.product_id):
-            if not self._match_all_variant_values(record.apply_on_ptav_ids):
+            # This is basically a straight clone from mrp, product.template _match_all_variant_values method
+            # https://github.com/odoo/odoo/blob/15.0/addons/mrp/models/product.py#L304
+            # Copied so that we do not need to depend on `mrp`.
+            #
+            # The intersection of the values of the product and those of the line satisfy:
+            # * the number of items equals the number of attributes (since a product cannot
+            #   have multiple values for the same attribute),
+            # * the attributes are a subset of the attributes of the line.
+            matches_all = len(self.product_template_attribute_value_ids & record.apply_on_ptav_ids) == len(record.apply_on_ptav_ids.attribute_id)
+            if not matches_all:
                 to_remove |= record
 
         return res - to_remove
