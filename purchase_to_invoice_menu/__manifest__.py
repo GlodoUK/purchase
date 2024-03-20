cb83cb4de58cf2d6ff0ffad6d7bb7f55c9bfa46c
Edwin Blommaerts <edwin.blommaerts@gmail.com>
2024-03-20 13:58:41 +0000

    20 Mar 2024, 13:58

---
+++ purchase_to_invoice_menu/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "purchase_to_invoice_menu",
+    "summary": "Adds a 'To Invoice' menu to the Purchase menu",
+    "author": "Glo Networks",
+    "website": "https://github.com/GlodoUK/purchase",
+    "category": "Uncategorized",
+    "version": "15.0.1.1.0",
+    "depends": ["purchase"],
+    "data": [
+        "views/menu.xml",
+    ],
+    "demo": [],
+    "license": "AGPL-3",
+}
